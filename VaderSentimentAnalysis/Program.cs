2bd2117ca153b584ef706817d8e7a394d017c7a7
Dael Separa <dael.separa@gmail.com>
2019-06-04 11:40:18 +0800

    analyze sentiment for texts from command line input

--- VaderSentimentAnalysis/Program.cs
+++ VaderSentimentAnalysis/Program.cs
@@ -6,16 +6,34 @@ namespace VaderSentimentAnalysis
     {
         public static void Main(string[] args)
         {
-            var sentimentText = "The plot was good, but the characters are uncompelling and the dialog is not great.";
-
-            sentimentText.Trim();
-
+            var sentimentText = "";
             var analyzer = new Analyzer("vader_lexicon.txt");
 
-            var sentiment = analyzer.GetSentimentScore(sentimentText);
+            foreach (var arg in args)
+            {
+                GetString(arg, "/TEXT=", ref sentimentText);
 
-            Console.WriteLine("Sentiment text: {0}", sentimentText);
-            Console.WriteLine("Sentiment: ['neg' => {0:0.000}, 'neu' => {1:0.000}, 'pos' => {2:0.000}, 'compound' => {3:0.0000}]", sentiment.Item1, sentiment.Item2, sentiment.Item3, sentiment.Item4);
+                sentimentText.Trim();
+
+                if (!string.IsNullOrEmpty(sentimentText))
+                {
+                    var sentiment = analyzer.GetSentimentScore(sentimentText);
+
+                    Console.WriteLine("\nSentiment text: {0}", sentimentText);
+                    Console.WriteLine("Sentiment: ['neg' => {0:0.000}, 'neu' => {1:0.000}, 'pos' => {2:0.000}, 'compound' => {3:0.0000}]", sentiment.Item1, sentiment.Item2, sentiment.Item3, sentiment.Item4);
+
+                    sentimentText = "";
+                }
+            }
+        }
+
+        static void GetString(string arg, string str, ref string dst)
+        {
+            if (arg.Length > 0 && str.Length > 0)
+            {
+                if (arg.Length > str.Length && string.Compare(arg.Substring(0, str.Length).ToUpper(), str, StringComparison.Ordinal) == 0)
+                    dst = arg.Substring(str.Length);
+            }
         }
     }
 }
